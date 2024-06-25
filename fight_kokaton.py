f1e5dacf9cdc6364bfcabc53995da86cc5aff291
c0b23158b7 <c0b23158b7@edu.teu.ac.jp>
2024-06-25 18:04:17 +0900

    行間統一 #1

--- fight_kokaton.py
+++ fight_kokaton.py
@@ -11,7 +11,6 @@ os.chdir(os.path.dirname(os.path.abspath(__file__)))
 NUM_OF_BOMBS = 5  # 爆弾の個数
 
 
-
 def check_bound(obj_rct: pg.Rect) -> tuple[bool, bool]:
     """
     オブジェクトが画面内or画面外を判定し，真理値タプルを返す関数
